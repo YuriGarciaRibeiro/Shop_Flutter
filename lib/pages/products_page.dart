1522338ddffcca85a8be05dd496d74faefeadc44
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2022-08-27 19:30:53 -0300

    Update products_page.dart

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:shop/components/app_drawer.dart';
 
 class ProductsPage extends StatelessWidget {
-  const ProductsPage ({Key? key}) : super(key: key);
+  const ProductsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Gerenciar produtos'),
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (ctx,i) => Text('elemento'),
+        ),
       ),
     );
   }
